28b4d89aee3fafb0ae1c5cafb1ac0fb02ff2ac08
ginpachi <m.fawaadali98@gmail.com>
2020-05-16 22:10:04 +0500

    Remove test lines from clRNG code

--- clRNG-usage/TestProgram/clRNGTest.c
+++ clRNG-usage/TestProgram/clRNGTest.c
@@ -1,9 +1,6 @@
 #include <clRNG/mrg31k3p.h>
-#include <clRNG/philox432.h>
 
 int main() {
-    clrngPhilox432Stream* stream = clrngPhilox432CreateStreams(10, 1, NULL, NULL);
-
     clrngMrg31k3pStream* streams = clrngMrg31k3pCreateStreams(NULL, 2, NULL, NULL);
     clrngMrg31k3pStream* single = clrngMrg31k3pCreateStreams(NULL, 1, NULL, NULL);
     int count = 0;
